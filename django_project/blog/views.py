32cabd7d493ba0db8cf4537ad88a3bfaedf0f29b
Lester <lester.deguzman@tup.edu.ph>
2019-12-26 14:49:42 +0800

    Templates & url redirecting in templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Lester De Guzman',
+        'title': 'A Blog Post',
+        'content': 'lorem ipsum dolor',
+        'date_posted': 'December 26, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'A Post',
+        'content': 'lorem ipsum dolor',
+        'date_posted': 'December 16, 2019'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About!</h1>')
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html')
