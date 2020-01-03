47c28241b8646f6ab70302597f35af46bbfc2c1b
Lester <lester.deguzman@tup.edu.ph>
2020-01-03 19:28:37 +0800

    CRUD using generic/class based views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,60 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import Post
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-        'title': 'Home'
-    }
-    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    # or use object_list in the template if you don't set up a context name.
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # `-` indicates newest to oldest.
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# LoginRequiredMixin is used in to class based views
+# while @loginrequired decorator is for function based views.
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # Test if the current user is equal to the post's author.
+    def test_func(self):
+        # A method of the update view to get the post's attribute.
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False # if False the response would be 403.
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    # Test if the current user is equal to the post's author.
+    def test_func(self):
+        # A method of the update view to get the post's attribute.
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     context = {
